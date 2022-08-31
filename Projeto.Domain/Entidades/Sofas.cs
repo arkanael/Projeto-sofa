86bcaba7115a703530250509f55c45fc510dc5b6
Luiz Guilherme Bandeira <arkanael@gmail.com>
2022-08-30 23:48:37 -0300

    Acertos no código

--- Projeto.Domain/Entidades/Sofas.cs
+++ Projeto.Domain/Entidades/Sofas.cs
@@ -11,16 +11,33 @@ namespace Projeto.Domain.Entidades
        
         public int Id { get; private set; }
         public string Nome { get; private set; }
+        public bool Avaliacao { get; private set; }
+        public int AvaliadoPor { get; set; }
+        public DateTime DatAvaliacao { get; set; }
 
-        public void SetSofa(string nome, bool inspetor )
+        public void Registrar(string nome, Usuario usuario )
         {
-            if (string.IsNullOrEmpty(nome))
+            
+            if (string.IsNullOrEmpty(usuario.Nome))
                 throw new Exception("Nome não pode ser vazio ou nulo");
 
-            if (inspetor)
-                throw new Exception($"O usuário {nome} não tem permissão para cadastrar um novo sofá");
+            if (usuario.Inspetor)
+                throw new Exception($"O usuário {usuario.Nome} não tem permissão para cadastrar um novo sofá");
+            
+            
+            Id = 1;
 
             Nome = nome;
+        }
+
+        public void Avaliar(bool avaliacao, Usuario usuario, int id)
+        {
+            if (usuario.Inspetor.Equals(false))
+                throw new Exception($"O usuário não tem permissão para avaliar o sofá");
+
+            Avaliacao = avaliacao;
+            DatAvaliacao = DateTime.Now;
+            AvaliadoPor = usuario.Id;
         }
     }
 }
--- Projeto.Tests/Entidades/SofaTest.cs
+++ Projeto.Tests/Entidades/SofaTest.cs
@@ -2,6 +2,7 @@
 using Projeto.Domain.Entidades;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,9 @@ namespace Projeto.Tests.Entidades
     [TestClass]
     public  class SofaTest
     {
+        List<Sofas> lista = new List<Sofas>();
+        
+
         [TestMethod]
         public void CadastarNovoSofa()
         {
@@ -27,15 +31,59 @@ namespace Projeto.Tests.Entidades
 
             try
             {
-                sofa.SetSofa("Sofa teste", usuario.Inspetor);
+                sofa.Registrar("Sofa teste", usuario);
             }
             catch (Exception e)
             {
                 Assert.AreEqual(sofa.Nome, "Sofa teste");
             }
 
-            
+
             Assert.AreEqual(sofa.Nome, "Sofa teste");
+
+        }
+
+
+        [TestMethod]
+        public void AvaliarSofa()
+        {
+
+
+            Usuario usuario = new Usuario();
+            usuario.Id = 1;
+            usuario.Nome = "Guilherme";
+            usuario.Login = "guilherme";
+            usuario.Senha = "1234";
+            usuario.Inspetor = false;
+
+            Usuario usuarioAvaliador = new Usuario();
+            usuarioAvaliador.Id = 2;
+            usuarioAvaliador.Nome = "Marcos";
+            usuarioAvaliador.Login = "marcos";
+            usuarioAvaliador.Senha = "1234";
+            usuarioAvaliador.Inspetor = true;
+
+            Sofas sofa = new Sofas();
+
+            try
+            {
+                sofa.Registrar("Sofa teste", usuario);
+                sofa.Avaliar(true, usuarioAvaliador, 1);
+                Debug.WriteLine($"Id: {sofa.Id}");
+                Debug.WriteLine($"Nome: {sofa.Nome}");
+                Debug.WriteLine($"Avaliado: {sofa.Avaliacao}");
+                Debug.WriteLine($"Avaliado Por: {usuarioAvaliador.Nome}");
+                Debug.WriteLine($"Data da avaliação: {sofa.DatAvaliacao}");
+
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(sofa.Nome, "Sofa teste");
+            }
+
+
+
+            Assert.IsTrue(sofa.Avaliacao);
 
         }
     }
